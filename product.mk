df26d2d678d16286c5cebc51242cf5c137d4f1d0
penglezos <panagiotisegl@gmail.com>
2024-02-24 18:38:36 +0200

    Inherit Certification makefile
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- product.mk
+++ product.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit Certification makefile
+$(call inherit-product-if-exists, vendor/certification/config.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
