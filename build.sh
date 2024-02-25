c20ea75cd82675995423c57a9c19a58021cd8431
penglezos <panagiotisegl@gmail.com>
2024-02-25 18:11:09 +0200

    build.sh: Rework script
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- build.sh
+++ build.sh
@@ -4,14 +4,30 @@
 #
 # Android Build script 
 #
+# Usage (in root of source):
+# 	./build.sh [options]
+#
 
 # Set defaults
-device='raphael'
-build_type='userdebug'
+DEVICE='raphael'
+BUILD_TYPE='userdebug'
+ZRAM_VALUE='32'
 export CCACHE_EXEC=$(command -v ccache)
 export CCACHE_DIR=$(pwd)/.ccache
 export USE_CCACHE=1
 ccache -M 50G
+
+clean () {
+    source build/envsetup.sh
+    lunch lineage_${DEVICE}-${BUILD_TYPE}
+    make clean && rm -rf out
+}
+
+installclean () {
+    source build/envsetup.sh
+    lunch lineage_${DEVICE}-${BUILT_TYPE}
+    make installclean
+}
 
 sync () {
     repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
@@ -22,7 +38,7 @@ sync () {
 
 zram () {
     sudo swapoff --all
-    sudo bash -c "echo 32G > /sys/block/zram0/disksize"
+    sudo bash -c "echo ${ZRAM_VALUE}G > /sys/block/zram0/disksize"
     sudo mkswap --label zram0 /dev/zram0
     sudo swapon --priority 32767 /dev/zram0
 }
@@ -46,40 +62,47 @@ patches () {
 
 build () {
     source build/envsetup.sh
-    lunch lineage_${device}-${build_type}
-    make bacon
+    lunch lineage_${DEVICE}-${BUILD_TYPE}
+    make bacon -j$(nproc --all)
 }
 
 kernel () {
     source build/envsetup.sh
-    lunch lineage_${device}-${build_type}
-    make bootimage
+    lunch lineage_${DEVICE}-${BUILD_TYPE}
+    make bootimage -j$(nproc --all)
 }
 
 recovery () {
     source build/envsetup.sh
-    lunch lineage_${device}-${build_type}
-    make recoveryimage
+    lunch lineage_${DEVICE}-${BUILD_TYPE}
+    make recoveryimage -j$(nproc --all)
 }
 
-echo -e "\n1.Sync sources\n2.Setup ZRAM\n3.Apply patches\n4.Build ROM\n5.Build kernel\n6.Build recovery"
-echo -e
-read -p "Your choice: " num
-case $num in 
-    1|2|3|4|5|6)
-esac
-
-if [ $num = '1' ]; then
-    sync
-elif [ $num = '2' ]; then
-    zram
-elif [ $num = '3' ]; then
-    patches
-elif [ $num = '4' ]; then
-    build
-elif [ $num = '5' ]; then
-    kernel
-elif [ $num = '6' ]; then
-    recovery
-else echo "Invalid input, aborting!"
+# Usage information when there are no arguments
+if [[ $# -eq 0 ]]; then
+	echo -e "\nUsage: ./build.sh [options]\n"
+	echo "Options:"
+    echo "  -c, --clean                    Clean entire build directory"
+    echo "  -i, --installclean             Dirty build"
+    echo "  -z, --zram                     Setup ZRAM"
+	echo "  -s, --sync                     Sync ROM and device sources"
+	echo "  -p, --patches                  Apply patches"
+	echo "  -b, --build                    Perform ROM build"
+	echo "  -k, --kernel                   Perform kernel build"
+	echo "  -r, --recovery                 Perform recovery build"
+	echo ""
+	exit 0
 fi
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do case $1 in
+	-c|--clean) clean;;
+	-i|--installclean) installclean;;
+    -z|--zram) zram;;
+	-s|--sync) sync;;
+	-p|--patches) patches;;
+	-b|--build) build;;
+	-k|--kernel) kernel;;
+    -r|--recovery) recovery;;
+	*) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
