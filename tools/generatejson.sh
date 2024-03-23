a733d72a0d9fac0d19c18ceb2b39740a306ab94b
penglezos <panagiotisegl@gmail.com>
2024-03-23 14:35:19 +0200

    Initial .json generator script
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

---
+++ tools/generatejson.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 ivanmeler <i_ivan@windowslive.com>
+# Copyright (C) 2024 Panagiotis Englezos <panagiotisegl@gmail.com>
+#
+# Android ROM .json file generator 
+#
+# Usage: ./generatejson.sh <ota zip>
+#
+
+if [ -z "$1" ] ; then
+    echo "Usage: $0 <ota zip>"
+    exit 1
+fi
+
+ROM="$1"
+
+METADATA=$(unzip -p "$ROM" META-INF/com/android/metadata)
+SDK_LEVEL=$(echo "$METADATA" | grep post-sdk-level | cut -f2 -d '=')
+TIMESTAMP=$(echo "$METADATA" | grep post-timestamp | cut -f2 -d '=')
+
+FILENAME=$(basename $ROM)
+DEVICE=$(echo $FILENAME | cut -f5 -d '-' | cut -f1 -d".")
+ROMTYPE=$(echo $FILENAME | cut -f4 -d '-')
+DATE=$(echo $FILENAME | cut -f3 -d '-')
+ID=$(echo ${TIMESTAMP}${DEVICE}${SDK_LEVEL} | sha256sum | cut -f 1 -d ' ')
+SIZE=$(du -b $ROM | cut -f1 -d '	')
+TYPE=$(echo $FILENAME | cut -f4 -d '-')
+VERSION=$(echo $FILENAME | cut -f2 -d '-')
+RELASE_TAG=lineage-${VERSION}-${DATE}-${ROMTYPE}-${DEVICE}
+
+URL="https://github.com/penglezos/device_xiaomi_raphael/releases/download/${RELASE_TAG}/${FILENAME}"
+
+response=$(jq -n --arg datetime $TIMESTAMP \
+        --arg filename $FILENAME \
+        --arg id $ID \
+        --arg romtype $ROMTYPE \
+        --arg size $SIZE \
+        --arg url $URL \
+        --arg version $VERSION \
+        '$ARGS.named'
+)
+wrapped_response=$(jq -n --argjson response "[$response]" '$ARGS.named')
+
+echo "$wrapped_response" > ../ota/$VERSION/$DEVICE.json
