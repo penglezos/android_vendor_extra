63ec9108dfffd55467cf37f6fb29d771dfb43b5b
penglezos <panagiotisegl@gmail.com>
2024-07-16 20:45:08 +0300

    fonts: Add Inter v4.0
    - Imported from: https://github.com/rsms/inter
    
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- product.mk
+++ product.mk
@@ -4,10 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/extra/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+    vendor/extra/fonts/InterVariable.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/InterVariable.ttf \
+    vendor/extra/fonts/InterVariable-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/InterVariable-Italic.ttf
+
 # Overlays
 PRODUCT_PACKAGES += \
     CertificationOverlay \
-    LineageUpdaterOverlay
+    LineageUpdaterOverlay \
+    FontInterOverlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
